b712be203176fab6cb8196403ba7bd12538a968a
CharlotteZhang21 <charlotte.zhang@vungle.com>
2019-01-16 14:05:29 +0000

    replace assets

--- seasonal template/CNY2019/dist/Vungle-Slots-Era-170420-VTP-615-IEC-EN-version--/settings.js
+++ seasonal template/CNY2019/dist/Vungle-Slots-Era-170420-VTP-615-IEC-EN-version--/settings.js
@@ -21,42 +21,5 @@ PiecSettings.assets = [
 
 PiecSettings.treesDisappear = false;
 
-// PiecSettings.portrait = {
-//     envelopeY: .72,
-//     contentsFinalWidth: 1.55, //in % (50% should be 0.5)
-//     contentsFinalY: -0.01,     //in %
-// };
-
-// PiecSettings.portraitTablet = {
-//     envelopeY: .72,
-//     contentsFinalWidth: 1.4, //in % (50% should be 0.5)
-//     contentsFinalY: 0,     //in %
-// };
-
-// PiecSettings.landscape = {
-//     envelopeY: .45,         // envelope vertical position
-//     contentsFinalWidth: .9,
-//     contentsFinalY: .09,     //in %
-// };
-
-// PiecSettings.landscapeTablet = {
-//     envelopeY: .55,
-//     contentsFinalWidth: .95, //in % (50% should be 0.5)
-//     contentsFinalY: .3,     //in %
-// };
-
-// PiecSettings.pngAnimations = [
-//     { // 1
-//         src: 'firework-ani.png',
-//         spriteWidth: 1024/4,
-//         spriteHeight: 1024/4,
-//         spriteNumber: 16,
-//         loops: 1,
-//         delay: 0,
-//         fps: 24,
-//         scale: 1,
-//         isReversed: false,
-//     }
-// ];
 
 PiecSettings.version = '-';
